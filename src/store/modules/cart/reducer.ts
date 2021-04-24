1702ae703d7fb504234870cf0fe513490c04a9e7
JoaoGuiBC <joaoguibc@gmail.com>
2021-04-24 17:10:33 -0300

    Added the ability to increase the quantity of the product in the state

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -15,10 +15,18 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
 
         const { product } = action.payload;
 
-        draft.items.push({
-          product,
-          quantity: 1,
-        });
+        const productInCartIndex = draft.items.findIndex(item => 
+          item.product.id === product.id,  
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
+        }
 
         break;
       }
